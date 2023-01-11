d117bfe0488efd8d0914dd911b1347b361719828
GauravPratap1988ster <115794844+GauravPratap1988ster@users.noreply.github.com>
2023-01-11 17:13:23 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -5,11 +5,15 @@ import typewise_alert
 class TypewiseTest(unittest.TestCase):
   def test_infers_breach_as_per_limits(self):
     self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
-    self.assertEqual(typewise_alert.infer_breach(150, 50, 100), 'TOO_HIGH')
-    self.assertEqual(typewise_alert.infer_breach(50, 50, 100), 'NORMAL')
-    self.assertEqual(typewise_alert.infer_breach(75, 50, 100), 'NORMAL')
-    self.assertEqual(typewise_alert.infer_breach(49, 50, 100), 'TOO_LOW')
-    self.assertEqual(typewise_alert.infer_breach(101, 50, 100), 'TOO_HIGH')
+    self.assertTrue(typewise_alert.infer_breach(-1, 0, 45) == 'TOO_LOW')
+    self.assertTrue(typewise_alert.infer_breach(1, 0, 45) == 'NORMAL')
+    self.assertTrue(typewise_alert.infer_breach(44, 0, 45) == 'NORMAL')
+    self.assertTrue(typewise_alert.infer_breach(46, 0, 45) == 'TOO_HIGH')
+    self.assertTrue(typewise_alert.infer_breach(40, 0, 40) == 'NORMAL')
+    self.assertTrue(typewise_alert.infer_breach(-1, 0, 40) == 'TOO_LOW')
+     
+ 
+
 
 if __name__ == '__main__':
   unittest.main()
