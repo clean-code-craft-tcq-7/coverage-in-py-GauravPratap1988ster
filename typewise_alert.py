b4a4609b62d3d8c80d838e8c9cd1428ee234d56d
GauravPratap1988ster <115794844+GauravPratap1988ster@users.noreply.github.com>
2023-01-12 13:16:37 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,4 +1,4 @@
-def infer_breach(value, lower_limit, upper_limit):
+ef infer_breach(value, lower_limit, upper_limit):
     if value < lower_limit:
         return 'TOO_LOW'
     if value > upper_limit:
